fd031180b84d87bcbc48d39d8e0e06fd6957fe65
Pericles <periclesebmendes@gmail.com>
2023-10-01 17:23:54 -0300

    feat: added prisma and database models

---
+++ prisma/migrations/20231001201653_models/migration.sql
@@ -0,0 +1,46 @@
+-- CreateTable
+CREATE TABLE "Participant" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "balance" INTEGER NOT NULL,
+
+    CONSTRAINT "Participant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "homeTeamName" TEXT NOT NULL,
+    "awayTeamName" TEXT NOT NULL,
+    "homeTeamScore" INTEGER NOT NULL,
+    "awayTeamScore" INTEGER NOT NULL,
+    "isFinished" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Bet" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "homeTeamScore" INTEGER NOT NULL,
+    "awayTeamScore" INTEGER NOT NULL,
+    "amountBet" INTEGER NOT NULL,
+    "gameId" INTEGER NOT NULL,
+    "participantId" INTEGER NOT NULL,
+    "status" TEXT NOT NULL,
+    "amountWon" INTEGER NOT NULL DEFAULT -1,
+
+    CONSTRAINT "Bet_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Bet" ADD CONSTRAINT "Bet_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Bet" ADD CONSTRAINT "Bet_participantId_fkey" FOREIGN KEY ("participantId") REFERENCES "Participant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
